e7681f57faf08794f2d9f849ccd7d1e1efedb5e4
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-12-17 18:58:08 +0000

    pylint: auto fixes

--- userbot/plugins/kk.py
+++ userbot/plugins/kk.py
@@ -23,7 +23,7 @@ async def _(event):
                 "قمر\nاقمار\nقمور\nنظام شمسي\nغبي\nتنويم\nحلويات\nساعه\nرياضه\nالارض\nنجمه\nمربعات\nمطر\nاحبك\nشرطه\nدائره\nانمي\nقرد\nايد\nقتل\nقنابل\nقلب\nقلبي\nقلوب\nحزين\nمحح\nتفكير\nتهكير\nترحيب\nمسح ترحيب\nلسته ترحيب\nحظر\nالغاء حظر\nالمحظورين\nكتم\nالغاء كتم\nرفع القيود"
             )
 
-            
+
 @bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
