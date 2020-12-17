9a80b868d52e3f8f6f4c2f1bb65dcb4e54a1905d
Angly <71581309+TH7RM@users.noreply.github.com>
2020-12-17 20:35:39 +0300

    Update kk.py

--- userbot/plugins/kk.py
+++ userbot/plugins/kk.py
@@ -11,10 +11,10 @@ async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    if input_str == "المطور":
-        r = random.randint(0, 1)
+    if input_str == "الاوامر":
+        r = random.randint(0, 0)
         logger.debug(r)
         if r == 0:
-            await event.edit("pikapikaa\n\n Developer》 @llllflf")
+            await event.edit("قلب\nقلبي\nقلوب\nقمر\nاقمار\nقمور\nنجمه\nنظام شمسي\nغبي\nطياره\nرياضه\nتنويم\nحلويات\nساعه\nمربعات\nاحبك\nانمي\nدائره\nقرد\nقنابل\nقتل\nحزين\nتفكير\nتهكير")
         else:
-            await event.edit("pikapikaa\n\n Developer》 ")
+            await event.edit("قلب\nقلبي\nقلوب\nقمر\nاقمار\nقمور\nنجمه\nنظام شمسي\nغبي\nطياره\nرياضه\nتنويم\nحلويات\nساعه\nمربعات\nاحبك\nانمي\nدائره\nقرد\nقنابل\nقتل\nحزين\nتفكير\nتهكير"")
