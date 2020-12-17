212c38a7379809b8f5b0ae0e1b49b757f15a58c8
Angly <71581309+TH7RM@users.noreply.github.com>
2020-12-17 20:08:13 +0300

    تحديث

---
+++ userbot/plugins/kk.py
@@ -0,0 +1,20 @@
+"""command: .kk By @Grandpaa_please """
+
+
+import random
+
+from telethon import events
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    if input_str == "المطور":
+        r = random.randint(0, 3)
+        logger.debug(r)
+        if r == 0:
+            await event.edit("pikapikaa\n\n Developer》 @llllflf")
+        else:
+            await event.edit("pikapikaa\n\n Developer》 @llllflf")
