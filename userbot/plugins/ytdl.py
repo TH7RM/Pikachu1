31077d451f309db37fc416fea74e0c4d05ae5115
Angly <71581309+TH7RM@users.noreply.github.com>
2020-12-28 09:33:31 +0300

    Update ytdl.py

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -27,8 +27,8 @@ from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 from . import CMD_HELP, hmention, progress, reply_id
 
 
-@bot.on(admin_cmd(pattern="yt(a|v) (.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="yt(a|v) (.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="يوتيو(a|ب) (.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="يوتيو(a|ب) (.*)", allow_sudo=True))
 async def download_video(v_url):
     """ For .ytdl command, download media from YouTube and many other sites. """
     url = v_url.pattern_match.group(2)
@@ -57,7 +57,7 @@ async def download_video(v_url):
         }
         video = False
         song = True
-    elif ytype == "v":
+    elif ytype == "ب":
         opts = {
             "format": "best",
             "addmetadata": True,
